3179d439ef4399bed4bdddfd16bda4a34abea5ec
babygramps <pocolypz@gmail.com>
2025-06-02 21:30:17 -0700

    feat: update Next.js configuration for development environment
    Added allowedDevOrigins to the Next.js configuration to specify permitted origins for development. Updated the dev script in package.json to bind the development server to all network interfaces (0.0.0.0), enhancing accessibility during local development.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    '172.17.112.1',
+    '192.168.4.101:3000',
+    '192.168.4.25',
+    'localhost',
+    '127.0.0.1',
+  ],
 };
 
 export default nextConfig;
